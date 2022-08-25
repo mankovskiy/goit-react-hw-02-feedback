a1c4ef9bbf9acd4dd88dee7c5ccd742436290a7d
Stanislav Mankovskyi <odnorazka25@gmail.com>
2022-08-25 17:36:28 +0100

    correct-hw-2-react

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import { Component } from 'react';
 import { Section } from '../components/Section/Section';
 import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
 import { Statistics } from './Statistics/Statistics';
+import { Notification } from 'Notification/Notification';
 export class App extends Component {
   state = {
     good: 0,
@@ -24,22 +25,29 @@ export class App extends Component {
   };
   render() {
     const { good, neutral, bad } = this.state;
+
+    const total = this.countTotalFeedback();
+
     return (
       <div>
-        <Section title={'Please leave feedbac'}>
+        <Section title={'Please leave feedback'}>
           <FeedbackOptions
             options={Object.keys(this.state)}
             onLeaveFeedback={this.onLeaveFeedback}
           />
         </Section>
         <Section title={'Statistics'}>
-          <Statistics
-            good={good}
-            neutral={neutral}
-            bad={bad}
-            total={this.countTotalFeedback()}
-            positivePercentage={this.countPositiveFeedbackPercentage()}
-          />
+          {total ? (
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={total}
+              positivePercentage={this.countPositiveFeedbackPercentage()}
+            />
+          ) : (
+            <Notification message="There is no feedback" />
+          )}
         </Section>
       </div>
     );
