8ded97f700a3e0982229d1aa8e95cecb60ed8b87
Stanislav Mankovskyi <odnorazka25@gmail.com>
2022-08-27 13:04:26 +0100

    slash

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 import { Component } from 'react';
 import { Section } from '../components/Section/Section';
-import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
+import { FeedbackOptions } from '../components/FeedbackOptions/FeedbackOptions';
 import { Statistics } from '../components/Statistics/Statistics';
 import { Notification } from '../components/Notification/Notification';
 export class App extends Component {
